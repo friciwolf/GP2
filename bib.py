c074e8cb6a91e2f3dce51e2105023a45ba2bfa8d
Mate <friciwolf@gmail.com>
2019-02-26 17:09:49 +0100

    Update bib.py

--- bib.py
+++ bib.py
@@ -9,7 +9,6 @@ Created on Tue Feb 26 13:26:54 2019
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.gridspec import GridSpec
-
 import praktikum.analyse as anal
 
 def c_open(file):
@@ -50,7 +49,7 @@ def pltmitres(x,y,ex,ey, xl, yl, xeinheit, yeinheit, titel):
     
     plt.subplot(gs1[:-1,:-1])
     plt.title(titel)
-    plt.errorbar(x,y, ex, ey, marker="x", linestyle="None", capsize=5)
+    plt.errorbar(x,y, ey, ex, marker="x", linestyle="None", capsize=5)
     l = max(x)-min(x)
     x2 = np.arange(min(x)-l*0.1, max(x)+l*0.1, l/1000)
     y2 = a*x2+b
@@ -59,15 +58,8 @@ def pltmitres(x,y,ex,ey, xl, yl, xeinheit, yeinheit, titel):
     plt.legend(title="Lineare Regression\n{} = ({:.2f} ± {:.2f}){} $\cdot$ {}+({:.2f}±{:.2f}){}\n$\chi^2 /NDF={:.2f}$".format(yl,a,ea, xeinheit,xl,b, eb, yeinheit, chiq/(len(x)-2)), loc=1)
     
     plt.subplot(gs1[2, :-1])
-    plt.errorbar(x,y-y, np.sqrt(ex**2*a+ey**2), marker="x", linestyle="None", capsize=5)
+    plt.errorbar(x,y-a*x-b, np.sqrt(ex**2*a**2+ey**2), marker="x", linestyle="None", capsize=5)
     plt.axhline(0, color="orange")
     plt.xlabel(xl+" [{}]".format(yeinheit))
     
     plt.tight_layout()
-    
-x = np.arange(0.1,1,0.1)
-y = np.sin(x)
-ex = np.ones(len(x))*0.5
-ey = np.ones(len(y))*0.2
-pltmitres(x,y, ex, ey, "x", "y", "m/s", "s", "titel")
-plt.show()
